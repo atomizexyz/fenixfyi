3bd1a9bcd4a5d18ca194b51275882206ff8e00d4
Joe Blau <im@joeblau.com>
2023-04-12 20:45:23 -0700

    feat: fix footer

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -4,16 +4,16 @@ export default function Footer() {
   return (
     <footer>
       <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
-        <nav className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
+        <nav className="-mb-6 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
           {navigation.footer.map((item) => (
-            <div key={item.name} className="pb-6">
+            <div key={item.name} className="pb-16 flex justify-center">
               <a href={item.href} className="text-sm leading-6 primary-link">
                 {item.name}
               </a>
             </div>
           ))}
         </nav>
-        <div className="mt-10 flex justify-center space-x-10">
+        <div className="mt-10 flex justify-center space-x-16">
           {navigation.social.map((item) => (
             <a key={item.name} href={item.href} className="secondary-link">
               <span className="sr-only">{item.name}</span>
