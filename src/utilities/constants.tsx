0c528c49b8fc904b9e0bed4da00396958d0bb356
Joe Blau <im@joeblau.com>
2023-04-14 22:28:43 -0700

    feat: update max stake length

--- src/utilities/constants.tsx
+++ src/utilities/constants.tsx
@@ -1,6 +1,4 @@
-import { ethers } from "ethers";
-
-export const FENIX_MAX_STAKE_LENGTH = 7665;
+export const FENIX_MAX_STAKE_LENGTH = 7777;
 export const UTC_TIME = new Date().getTime() / 1000;
 export const ONE_DAY_TS = 86400;
 export const ONE_EIGHTY_DAYS_TS = 180 * ONE_DAY_TS;
