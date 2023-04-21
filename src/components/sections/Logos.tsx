011fa63035d3c9d9a30b3064f2ad72b86cffc780
Joe Blau <im@joeblau.com>
2023-04-21 09:45:35 -0700

    feat: add xenmon

--- src/components/sections/Logos.tsx
+++ src/components/sections/Logos.tsx
@@ -7,7 +7,7 @@ export const Logos = () => {
       <h2 className="text-center text-lg font-semibold leading-8 secondary-text">
         FENIX is part of the XEN ecosystem, a collection of projects that are building the future of DeFi.
       </h2>
-      <div className="mx-auto mt-10 grid max-w-lg grid-cols-1 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-3 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+      <div className="mx-auto mt-10 grid max-w-lg grid-cols-1 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-2 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-4">
         {communityProjects.map((project) => (
           <Link href={project.href} className="flex flex-col items-center" key={project.name}>
             <project.logo className="primary-text" aria-hidden="true" />
