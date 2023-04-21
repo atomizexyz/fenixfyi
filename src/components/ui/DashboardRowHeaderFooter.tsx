797b60d34871ac6107b9b29e62b507020a0750dd
Joe Blau <im@joeblau.com>
2023-04-21 16:47:04 -0700

    fix: headers

--- src/components/ui/DashboardRowHeaderFooter.tsx
+++ src/components/ui/DashboardRowHeaderFooter.tsx
@@ -2,30 +2,27 @@ export default function DashboardRowHeaderFooter() {
   return (
     <tr>
       <th scope="col" className="py-3.5 pl-4 pr-3 text-left align-text-top text-sm font-semibold primary-text sm:pl-6">
-        Start
+        Chain
       </th>
-      <th scope="col" className="py-3.5 pl-4 pr-3 text-left align-text-top text-sm font-semibold primary-text sm:pl-6">
-        End
+      <th scope="col" className="px-3 py-3.5 text-left align-text-top text-sm font-semibold primary-text">
+        Status
       </th>
       <th scope="col" className="px-3 py-3.5 text-right align-text-top text-sm font-semibold primary-text">
-        Principal
+        Equity Supply
         <div className="text-xs tertiary-text"> FENIX </div>
       </th>
       <th scope="col" className="px-3 py-3.5 text-right align-text-top text-sm font-semibold primary-text">
-        Shares
-      </th>
-      <th scope="col" className="px-3 py-3.5 text-right align-text-top text-sm font-semibold primary-text">
-        Penalty
-      </th>
-      <th scope="col" className="px-3 py-3.5 text-right align-text-top text-sm font-semibold primary-text">
-        Payout
+        Reward Supply
         <div className="text-xs tertiary-text"> FENIX </div>
       </th>
-      <th scope="col" className="px-3 py-3.5 text-center align-text-top text-sm font-semibold primary-text">
-        Progress
+      <th scope="col" className="px-3 py-3.5 text-right align-text-top text-sm font-semibold primary-text">
+        Share Rate
+      </th>
+      <th scope="col" className="px-3 py-3.5 text-left align-text-top text-sm font-semibold primary-text">
+        Address
       </th>
       <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
-        <span className="sr-only">Actions</span>
+        <span className="sr-only">Edit</span>
       </th>
     </tr>
   );
