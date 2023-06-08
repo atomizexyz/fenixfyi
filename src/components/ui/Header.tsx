e77c0fb8a02896366705a8c4b8cb669fc26cf567
Joe Blau <im@joeblau.com>
2023-06-08 06:04:25 -0700

    fix: reward link in mobile

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -192,7 +192,7 @@ export default function Header() {
                 </div>
                 <div className="space-y-2 py-6">
                   <a
-                    href="#"
+                    href="/reward"
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 primary-link"
                   >
                     Reward
