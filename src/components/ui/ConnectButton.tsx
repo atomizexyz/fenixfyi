7e8a45d42f4b7685562d269b2f79b2aae7779824
Joe Blau <im@joeblau.com>
2023-12-14 06:59:28 -0800

    feat: remove connect button

--- src/components/ui/ConnectButton.tsx
+++ src/components/ui/ConnectButton.tsx
@@ -5,25 +5,26 @@ import { ConnectKitButton } from "connectkit";
 
 export default function ConnectButton() {
   return (
-    <ConnectKitButton.Custom>
-      {({ isConnected, show, address, truncatedAddress, chain }) => {
-        return (
-          <>
-            {isConnected ? (
-              <button onClick={show} className="primary-button">
-                <div className="flex space-x-4 items-center">
-                  {chain && chainIcons[chain.id]}
-                  <div className="font-mono">{truncatedAddress}</div>
-                </div>
-              </button>
-            ) : (
-              <button onClick={show} className="primary-button">
-                Connect Wallet
-              </button>
-            )}
-          </>
-        );
-      }}
-    </ConnectKitButton.Custom>
+    <div className="primary-text">Maintenance</div>
+    // <ConnectKitButton.Custom>
+    //   {({ isConnected, show, address, truncatedAddress, chain }) => {
+    //     return (
+    //       <>
+    //         {isConnected ? (
+    //           <button onClick={show} className="primary-button">
+    //             <div className="flex space-x-4 items-center">
+    //               {chain && chainIcons[chain.id]}
+    //               <div className="font-mono">{truncatedAddress}</div>
+    //             </div>
+    //           </button>
+    //         ) : (
+    //           <button onClick={show} className="primary-button">
+    //             Connect Wallet
+    //           </button>
+    //         )}
+    //       </>
+    //     );
+    //   }}
+    // </ConnectKitButton.Custom>
   );
 }
