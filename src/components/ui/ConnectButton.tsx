5c1eab216aef7784b6be049924be538ec80ebe62
Joe Blau <im@joeblau.com>
2023-12-14 09:13:07 -0800

    feat: connectkit does not use ledger library

--- src/components/ui/ConnectButton.tsx
+++ src/components/ui/ConnectButton.tsx
@@ -5,26 +5,25 @@ import { ConnectKitButton } from "connectkit";
 
 export default function ConnectButton() {
   return (
-    <div className="primary-text">Maintenance</div>
-    // <ConnectKitButton.Custom>
-    //   {({ isConnected, show, address, truncatedAddress, chain }) => {
-    //     return (
-    //       <>
-    //         {isConnected ? (
-    //           <button onClick={show} className="primary-button">
-    //             <div className="flex space-x-4 items-center">
-    //               {chain && chainIcons[chain.id]}
-    //               <div className="font-mono">{truncatedAddress}</div>
-    //             </div>
-    //           </button>
-    //         ) : (
-    //           <button onClick={show} className="primary-button">
-    //             Connect Wallet
-    //           </button>
-    //         )}
-    //       </>
-    //     );
-    //   }}
-    // </ConnectKitButton.Custom>
+    <ConnectKitButton.Custom>
+      {({ isConnected, show, address, truncatedAddress, chain }) => {
+        return (
+          <>
+            {isConnected ? (
+              <button onClick={show} className="primary-button">
+                <div className="flex space-x-4 items-center">
+                  {chain && chainIcons[chain.id]}
+                  <div className="font-mono">{truncatedAddress}</div>
+                </div>
+              </button>
+            ) : (
+              <button onClick={show} className="primary-button">
+                Connect Wallet
+              </button>
+            )}
+          </>
+        );
+      }}
+    </ConnectKitButton.Custom>
   );
 }
